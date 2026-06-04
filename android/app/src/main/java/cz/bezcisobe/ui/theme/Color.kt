88bec7b2d6a5622fadd06eef85b2099e83f9102e
Iva Fischerova <fischerova.ivka@gmail.com>
2026-06-04 15:43:32 +0200

    feat(android): Material3 theme with light/dark brand colors

--- android/app/src/main/java/cz/bezcisobe/ui/theme/Color.kt
+++ android/app/src/main/java/cz/bezcisobe/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BrandGreen = Color(0xFF2E7D32)
+val BrandGreenDark = Color(0xFF1B5E20)
+val BrandViolet = Color(0xFF7C4DFF)
+val BrandSky = Color(0xFF40C4FF)
